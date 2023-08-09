d03d91f9e04b2aee044d89d6c56cf5504a23bf9f
Will Royal <william.j.royal@gmail.com>
2023-08-08 21:33:25 -0700

    initial commit

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Number guessing game
+            var favoriteNumber = 42;
+            int userInput;
+            do
+            {
+                Console.WriteLine("Try to guess my favorite number.");
+                userInput = int.Parse(Console.ReadLine());
+
+                if (userInput < favoriteNumber)
+                {
+                    Console.WriteLine($"{userInput} is too low");
+                }
+                else if (userInput > favoriteNumber)
+                {
+                    Console.WriteLine($"{userInput} is too high");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it!");
+                }
+            }
+            while (userInput != favoriteNumber);
         }
     }
 }
